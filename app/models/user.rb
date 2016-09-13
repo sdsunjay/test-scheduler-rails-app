9463357923f3bddd392dd262f3fe499ee9ac916f
Sunjay Dhama <sdsunjay73@yahoo.com>
2016-09-12 19:07:04 -0700

    small fix with users name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
   has_many :bids
   has_many :posts, through: :bids, dependent: :destroy
+  validates :email, presence: {message: 'You must enter your email'}
+  validates :name, presence: {message: 'You must enter your name'}
 end
