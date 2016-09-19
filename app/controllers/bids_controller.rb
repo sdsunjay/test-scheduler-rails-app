97294a69005e17dab3f449a6a3a62a4ce5c4c352
Aaron Reisman <aaron@lifeiscontent.net>
2016-09-20 02:03:50 +0700

    Cleanup bid logic in controllers

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -10,11 +10,11 @@ class BidsController < ApplicationController
   end
 
   def index
-      @bids = Bid.where('post_id' => @post).order("created_at")
+    @bids = Bid.where(post_id: @post.id).order(:created_at)
   end
 
   def new
-      @bid = Bid.new
+    @bid = Bid.new
   end
 
   def edit
@@ -22,45 +22,43 @@ class BidsController < ApplicationController
 
   def create
     if current_user.id != @post.user_id
-        if @post.status == 'open' || @post.status == 'pending'
-            @bid = @post.bids.build(bid_params)
-            @bid.user = current_user
-            # Save the bid
-            if @bid.save
-                flash[:notice] = 'Bid Placed'
-                if @post.status == 'open'
-                    @post.status == 'pending'
-                end
-
-                if @post.save
-                    redirect_to @post
-                else
-                    flash[:alert] = 'Post status NOT changed'
-                end
-            else
-              flash[:alert] = 'Bid NOT Placed'
+      if @post.open? || @post.pending?
+        @bid = @post.bids.build(bid_params.merge(user_id: current_user.id))
+        # Save the bid
+        if @bid.save
+          flash[:notice] = 'Bid Placed'
+          # This logic should probably live in an activerecord callback
+          if @post.status == 'open'
+            @post.status == 'pending'
           end
+
+          if !@post.save
+            flash[:alert] = 'Post status NOT changed'
+          end
+        else
+          flash[:alert] = 'Bid NOT Placed'
+        end
       else
-          flash[:alert] = 'Not open for bidding'
+        flash[:alert] = 'Not open for bidding'
       end
     else
-        flash[:alert] = 'You cannot bid on your own auction'
+      flash[:alert] = 'You cannot bid on your own auction'
     end
 
-
+    redirect_to @post
   end
 
   private
 
   def set_bid
-      @bid = Bid.find(params[:id])
+    @bid = Bid.find(params[:id])
   end
+
   def set_post
-      @post = Post.find(params[:post_id])
+    @post = Post.find(params[:post_id])
   end
 
   def bid_params
-      params.require(:bid).permit(:amount)
+    params.require(:bid).permit(:amount)
   end
-
 end
--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,15 +1,15 @@
 class Bid < ActiveRecord::Base
-    delegate :email, to: :user, prefix: true
-    delegate :name, to: :user, prefix: true
-    belongs_to :post
-    belongs_to :user
-    validates_presence_of :amount
-    validates_numericality_of :amount 
-    validates_presence_of :user_id
-    validates_presence_of :post_id
-    
-    def self.most_recent
-        order("created_at").last
-    end
-    
+  delegate :email, to: :user, prefix: true
+  delegate :name, to: :user, prefix: true
+  belongs_to :post
+  belongs_to :user
+  validates_presence_of :amount
+  validates_numericality_of :amount
+  validates_presence_of :user_id
+  validates_presence_of :post_id
+
+  def self.most_recent
+    order(:created_at).last
+  end
+
 end
