8cc89d27a0b9de460f07d9176de7cec825473a65
Sunjay Dhama <sdsunjay73@yahoo.com>
2016-10-12 18:37:31 -0700

    Added a validation to prohibit users from placing bids lower than the current highest bid

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -7,9 +7,22 @@ class Bid < ActiveRecord::Base
   validates_numericality_of :amount
   validates_presence_of :user_id
   validates_presence_of :post_id
+  
+  validate :bid_amount_value
+
+  after_save :update_item
+  after_destroy :update_item
 
   def self.most_recent
     order(:created_at).last
+  end
+
+  def update_item
+      self.post.check_highest_bid
+  end
+
+  def bid_amount_value
+      errors.add(:amount, "Must be higher than current bid") unless self.post.price < self.amount
   end
 
 end
