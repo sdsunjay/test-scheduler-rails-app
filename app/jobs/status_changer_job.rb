2fc74ba39ab8d5de1d481358c349d1b13cb8d3a8
Sunjay Dhama <sdsunjay73@yahoo.com>
2016-12-12 22:47:59 -0800

    Moved logic into job, changed from find_each to where clause

--- app/jobs/status_changer_job.rb
+++ app/jobs/status_changer_job.rb
@@ -2,6 +2,19 @@ class StatusChangerJob < ActiveJob::Base
   queue_as :default
 
   def perform()
-      Post.find_each(&:update_status)
-  end
+       @posts = Post.where("(status = ? OR status = ?) AND when_date < ?", 0, 1, Date.today)
+       @posts.each do |post|
+       #Post.find_each do |post|
+          if post.status == 'pending'
+            # More than 0 bids and the auction has ended
+            post.update_attribute :status, 'complete'
+            Rails.logger.debug "#{self.class.name}: changed status to complete"
+            # update_attribute(post.status, 'complete')
+          elsif post.status == 'open'
+            # 0 bids and the auction has ended
+            post.update_attribute :status, 'incomplete'
+            Rails.logger.debug "#{self.class.name}: changed status to incomplete"
+        end
+     end
+   end
 end
