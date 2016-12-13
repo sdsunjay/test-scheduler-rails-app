f151dbbd6c512a3d516c8a4b55ebce3d9b5ab2ae
Sunjay Dhama <sdsunjay73@yahoo.com>
2016-12-12 23:08:10 -0800

    Check year, month, day, hour, minute, and second of post to determine if it has passed

--- app/jobs/status_changer_job.rb
+++ app/jobs/status_changer_job.rb
@@ -1,20 +1,20 @@
 class StatusChangerJob < ActiveJob::Base
   queue_as :default
+  require 'date'
 
   def perform()
-       @posts = Post.where("(status = ? OR status = ?) AND when_date < ?", 0, 1, Date.today)
-       @posts.each do |post|
-       #Post.find_each do |post|
-          if post.status == 'pending'
-            # More than 0 bids and the auction has ended
-            post.update_attribute :status, 'complete'
-            Rails.logger.debug "#{self.class.name}: changed status to complete"
-            # update_attribute(post.status, 'complete')
-          elsif post.status == 'open'
-            # 0 bids and the auction has ended
-            post.update_attribute :status, 'incomplete'
-            Rails.logger.debug "#{self.class.name}: changed status to incomplete"
-        end
-     end
-   end
+    @posts = Post.where("(status = ? OR status = ?) AND when_date < ?", 0, 1, DateTime.now)
+    @posts.each do |post|
+      if post.status == 'pending'
+        # More than 0 bids and the auction has ended
+        post.update_attribute :status, 'complete'
+        # Rails.logger.debug "#{self.class.name}: changed status to complete"
+      elsif post.status == 'open'
+        # 0 bids and the auction has ended
+        post.update_attribute :status, 'incomplete'
+        # Rails.logger.debug "#{self.class.name}: changed status to incomplete"
+      end
+    end
+  end
+
 end
